ec30292bb53e4e01a63307e42cb97f02bc0de21a
zyn <20281235@bjtu.edu.cn>
2023-05-13 21:07:11 +0800

    第二次提交
    根据实际需求，将重复检查时间改为一个小时。添加了封装.py文件的说明文件

--- autoweblogin.py
+++ autoweblogin.py
@@ -1,13 +1,14 @@
 import re
 import requests
 from time import strftime
-import time
+import time, datetime
 
 # URL地址
-schoolWebURL = 'http://10.10.42.3'  # 或者是'http://10.10.43.3'
+schoolWebURL1 = 'http://10.10.42.3'
+schoolWebURL2 = 'http://10.10.43.3'
 
 while (True):
-    response = requests.get(schoolWebURL)
+    response = requests.get(schoolWebURL1)
 
     # 正则表达式，匹配<title>标签中的内容
     pattern = re.compile('<title>(.*?)</title>', re.S)
@@ -15,17 +16,22 @@ while (True):
     title = title[0]  # 将格式转为字符串
 
     if title == '注销页':
-        print('%s %s 连接正常' % (strftime('%Y-%m-%d'), strftime('%H:%M:%S')))
-        print("状态码{}".format(response))  # 打印状态码
-        time.sleep(5)
+        now_date = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        print(now_date, "\n*******", '连接正常 :)')
+        print("*******", "状态码{}".format(response))  # 打印状态码
+        next_date = (datetime.datetime.now()+datetime.timedelta(seconds=3600)).strftime("%Y-%m-%d %H:%M:%S")
+        print("*******", "下一次检测将在：", next_date)
+        print("*******", "正在等待下一次检测...\n")
+        time.sleep(3600)  #每60min检查一次
         pass
 
     else:
         user = '20281235'  # 学号
         p = '314618'  # 密码
         t = str(int(round(time.time() * 1000)))  # 毫秒级时间戳
-        schoolWebLoginURL = schoolWebURL + '/drcom/login?callback=dr' + t + '&DDDDD=' + user + '&upass=' + p + '&0MKKey=123456&R1=0&R3=0&R6=0&para=00&v6ip=&_=' + t
-        print('%s %s 登录成功' % (strftime('%Y-%m-%d'), strftime('%H:%M:%S')))
-        print("状态码{}".format(response))  # 打印状态码
+        schoolWebLoginURL = schoolWebURL1 + '/drcom/login?callback=dr' + t + '&DDDDD=' + user + '&upass=' + p + '&0MKKey=123456&R1=0&R3=0&R6=0&para=00&v6ip=&_=' + t
+        print('%s %s' % (strftime('%Y-%m-%d'), strftime('%H:%M:%S')))
+        print("*******", '登录成功 :)')
+        print("*******", "状态码{}".format(response))  # 打印状态码
         requests.get(schoolWebLoginURL)
-
+        print("*******", "正在进入循环检测...\n")
