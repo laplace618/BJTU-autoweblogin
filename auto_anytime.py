ccf5b6572ff5d5e595f87d335073707e0b95ca5d
zyn <20281235@bjtu.edu.cn>
2024-04-26 15:22:40 +0800

    20240426 update adjust the 'user' & 'p'

--- auto_anytime.py
+++ auto_anytime.py
@@ -1,22 +1,31 @@
-import re
 import requests
+import datetime
+import time
+import re
 from time import strftime
-import time, datetime
+
 
 # URL地址
 schoolWebURL1 = 'http://10.10.42.3'
 schoolWebURL2 = 'http://10.10.43.3'
+user = '20281235'  # 学号
+p = '314618'  # 密码
 
 while (True):
-    response = requests.get(schoolWebURL1)
-
+    now_date = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    try:
+        response = requests.get(schoolWebURL1)
+    except requests.exceptions.RequestException:
+        print(now_date, "\n*******", '连接异常 :(')
+        print("*******", '网络连接已断开，请检查您的连接 :()')
+        time.sleep(60)  # 等待60秒后重新尝试连接
+        continue
     # 正则表达式，匹配<title>标签中的内容
     pattern = re.compile('<title>(.*?)</title>', re.S)
     title = re.findall(pattern, response.text)
     title = title[0]  # 将格式转为字符串
 
     if title == '注销页':
-        now_date = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         print(now_date, "\n*******", '连接正常 :)')
         print("*******", "状态码{}".format(response))  # 打印状态码
         # print("*******", "正在等待下一次检测...\n")
@@ -24,13 +33,12 @@ while (True):
         pass
 
     else:
-        user = '20281235'  # 学号
-        p = '314618'  # 密码
         t = str(int(round(time.time() * 1000)))  # 毫秒级时间戳
         schoolWebLoginURL = schoolWebURL1 + '/drcom/login?callback=dr' + t + '&DDDDD=' + user + '&upass=' + p + '&0MKKey=123456&R1=0&R3=0&R6=0&para=00&v6ip=&_=' + t
+        requests.get(schoolWebLoginURL)
         print('%s %s' % (strftime('%Y-%m-%d'), strftime('%H:%M:%S')))
         print("*******", '登录成功 :)')
         print("*******", "状态码{}".format(response))  # 打印状态码
-        requests.get(schoolWebLoginURL)
         # print("*******", "正在进入循环检测...\n")
         input("******* 请按下回车来执行新的检测...\n")
+        break
